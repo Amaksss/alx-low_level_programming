35a785b701806781d183a766252d89d4eab361a6
Amaksss <amakaorabuchi@gmail.com>
2023-06-15 14:41:11 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/**
+ * This program prints using the function puts and not printf
+ * Return 1
+ */
 int main() {
-	puts("Programming is like building a multilingual puzzl");
+	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }	
