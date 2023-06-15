0777e95e4b9be719d725b56189b5b88e4cd77ea9
Amaksss <amakaorabuchi@gmail.com>
2023-06-15 16:29:08 -0700

    print various size type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,12 @@
 
 int main(void)
 {
-	int x=1;
-        int y=4;
-	int z=8;
+	int x = 1;
+        int y = 4;
+	int z = 8;
 
-	printf("Size of a char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i bytes(s)\n" ,x ,y ,y ,z ,y);
+	printf("Size of a char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\n
+			Size of a long long int: %i byte(s)\nSize of a float: %i bytes(s)\n", x, y, y, z, y);
 
 	return (0);
 }
