b3259008623f015c3a3b51e06e5e37095ee21efa
Amaksss <amakaorabuchi@gmail.com>
2023-06-15 16:09:05 -0700

    print various size type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints various sizes of data types
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x=1
+        int y=4
+	int z=8
+
+	printf("Size of a char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i bytes(s)\n" ,x ,y ,y ,z ,y);
+
+	return (0);
+}
+
