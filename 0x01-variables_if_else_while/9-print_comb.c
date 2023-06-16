f1bc24baf4f77967aa93dcd49e64ca484a2f1e3a
Amaksss <amakaorabuchi@gmail.com>
2023-06-16 15:36:26 -0700

    possible combo of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,19 @@
 int main(void)
 {
 	int x;
+
 	for (x = 0; x < 10; ++x)
 {
 
 	putchar(x + '0');
 	if (x < 9)
-{
+      {
 	putchar(',');
 	putchar(' ');
-}
+      }
 
 }
-putchar('\n')
+putchar('\n');
 
 return (0);
 }
