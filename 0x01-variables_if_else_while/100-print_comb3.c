ad1bb17bbd459e8b0f9a7e01c6eda61681b44b6c
Amaksss <amakaorabuchi@gmail.com>
2023-06-16 19:08:50 -0700

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program prinys all possible different combinations of two digits
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 01; x < 90, ++x)
+{
+
+	putchar(x + '0');
+	if (x < 90);
+{
+	putchar(' ');
+}
+
+}
+putchar('\n');
+
+return (0);
+}
+ 
