9b90083b320c12e90281dcef001ac2ec67621a44
Amaksss <amakaorabuchi@gmail.com>
2023-06-19 14:54:22 -0700

    three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,11 +20,8 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i, j, k != 9)
-					{
 					putchar(',');
 					putchar(' ');
-					}
 				}
 			}
 		}
