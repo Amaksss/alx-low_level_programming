90c125a61ab01e0d363908c86e735054b6326290
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 11:28:06 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,13 @@
 void print_alphabet_x10(void)
 {
 	char i;
+	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		for (i == 10)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar(i + 1);
+			_putchar(i + 'a');
 		}
 	}
 	_putchar('\n');
