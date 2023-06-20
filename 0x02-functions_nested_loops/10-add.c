faafa2d2629bb52ff4a94779d572e7f488bad34f
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 10:15:32 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - entry point
+ * Return: 0 (success)
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+
+	int sum = a + b
+	{
+		_putchar("%d", sum);
+	}
+	return (0);
+}
