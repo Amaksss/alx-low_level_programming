77768ce139eb610a1a2001907cd12d6f263abed6
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 11:05:15 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * add - entry point
  * Return: 0 (success)
@@ -8,9 +9,9 @@ int main(void)
 	int a;
 	int b;
 
-	int add = a + b;
+	int result = a + b;
 	{
-		printf("%d", add);
+		printf("%d", result);
 	}
 	printf('\n');
 	return (0);
