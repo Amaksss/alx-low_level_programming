06a00da01014706c7cbbe2036ee970e3dbe26f14
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 16:36:33 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
  * _isalpha - Entry point
  * @c: parameter
  * Return: 0 or 1
  */
+
 int _isalpha(int c)
 {
-	if (c < 'A' || c > 'Z')
+	if (c < 'A' || c > 'z')
 	{
 		return (0);
 	}
