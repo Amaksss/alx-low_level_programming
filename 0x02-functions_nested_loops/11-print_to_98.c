104ab44ac15a923beea271bd82fb8ea2958dd907
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 15:55:39 -0700

    natural

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point
+ * print_to_98 - Entry point
  * Return: 0 (success)
  */
-int main(void)
+void print_to_98(int n)
 {
 	int n;
 
-	for (i = n; i<= 98; i++)
+	for (n = 0; n <= 98; n++)
 	{
 		_putchar(n);
 	}
+	_putchar('\n');
 	return (0);
 }
-
