d42cb6d5edf682a166dc123d65417ec9bc93e214
Amaksss <amakaorabuchi@gmail.com>
2023-06-20 16:12:39 -0700

    sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * add - Entry point
+ * @i: parameter
+ * @j: parameter
  * Return: 0 (success)
  */
 int add(int i, int j)
 {
 	int sum = i + j;
-	return sum;
+	return (sum);
 }
