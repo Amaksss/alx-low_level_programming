e5cb03469063b0ce14bcc0a0183205f96d8c026b
Amaksss <amakaorabuchi@gmail.com>
2023-06-22 08:18:34 -0700

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * more_numbers - entry point
  * followed by a new line
  */
+
 void more_numbers(void)
 {
 	int i;
 	int num;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 11; i++)
 	{
 		for (num = 0; num <= 14; num++)
+		{
+			for (int j = 0; j < 10; j++)
 		{
 			_putchar(num);
 		}
 	}
 	_putchar('\n');
+	}
 }
