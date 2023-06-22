b9631985b20fda995d64b067db5985eaf50788d4
Amaksss <amakaorabuchi@gmail.com>
2023-06-22 13:50:55 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -29,6 +29,15 @@ void print_line(int n)
 	}
 }
 
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
+
 /* _putchar function declaration statement */
 int _putchar(char c)
 {
