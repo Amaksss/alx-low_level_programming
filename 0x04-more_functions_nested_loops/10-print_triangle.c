57864998153c2089606597ecf4e101573a3562c9
Amaksss <amakaorabuchi@gmail.com>
2023-06-22 18:31:31 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,27 @@
 
 void print_triangle(int size)
 {
-	int i, j;
-
-
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= i; j++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
