d114666a2acc6c28249c2fe6b1fd87344c708333
Amaksss <amakaorabuchi@gmail.com>
2023-06-27 13:05:01 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int t = 0;
+	char *y = *str
+		int i;
+
+
+	while (*y != '\0')
+	{
+		y++;
+		length++;
+	}
+	t = length - 1;
+	for (i = 0; i <= t; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
