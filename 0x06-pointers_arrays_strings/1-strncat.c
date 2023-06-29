cbe9fd83133abc469b06ff3718e76ad5135eb752
Amaksss <amakaorabuchi@gmail.com>
2023-06-29 04:30:08 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,10 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-		while (dest[i] != '\0')
-		{
-			i++;
-		}
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
