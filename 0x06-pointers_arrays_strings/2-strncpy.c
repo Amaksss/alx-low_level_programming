8a48157bfcad152097bb1b9fa319b5ff1921f36e
Amaksss <amakaorabuchi@gmail.com>
2023-06-29 03:58:33 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - functions that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters
+ * Return: 0 (success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++
+	}
+	return (dest);
+}
