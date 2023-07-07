083413a896b27887096275d30723bb9b604e9843
Amaksss <amakaorabuchi@gmail.com>
2023-07-07 08:45:50 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  * Return: 0 (success)
  */
 
-int main(int argc_attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv);
+	printf("%s\n", *argv);
+
 	return (0);
 }
