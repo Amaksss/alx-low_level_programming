5e419715aa4b9c22cb08113dc995e25c5aa6f3f1
Amaksss <amakaorabuchi@gmail.com>
2023-07-17 07:20:55 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
 
 struct dog
 {
 	char *name;
 	float age;
-	char owner;
-};
-
-#endif
+	char *owner;
+}
