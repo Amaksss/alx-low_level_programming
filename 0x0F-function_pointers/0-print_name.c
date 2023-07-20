1b6aa78c7159a8d2ca2db24220eb559766b22d70
Amaksss <amakaorabuchi@gmail.com>
2023-07-19 17:35:44 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#include "function_pointers.h"
-=======
 #include "functions_pointers.h"
->>>>>>> cd788b8fc0be3897d6c10f038d7af422541dcef7
 #include <stdio.h>
 
 /**
@@ -11,10 +7,7 @@
  * @f: pointer to function
  * Return: nothing
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> cd788b8fc0be3897d6c10f038d7af422541dcef7
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
